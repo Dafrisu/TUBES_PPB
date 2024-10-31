9304fcab00e43b96f06e5a5205fdbbfde8fd59b7
zfersya <zfersya@gmail.com>
2024-10-31 11:27:17 +0700

    fix landing and login

--- lib/landing.dart
+++ lib/landing.dart
@@ -27,7 +27,7 @@ class landingPageState extends State<landingPage> {
             Text(
               'Selamat Datang',
               style: GoogleFonts.montserrat(
-                fontSize: 24, // Adjust font size as needed
+                fontSize: 24,
                 fontWeight: FontWeight.bold,
                 color: const Color.fromARGB(255, 0, 0, 0),
               ),
@@ -35,23 +35,26 @@ class landingPageState extends State<landingPage> {
             const Text(
               'di UMKMku',
               style: TextStyle(
-                fontSize: 24, // Adjust font size as needed
+                fontSize: 24, 
                 fontWeight: FontWeight.bold,
                 color: Color.fromARGB(255, 0, 0, 0),
               ),
             ),
-            const SizedBox(height: 20), // Space between text and image
+            const SizedBox(height: 20), 
             const Image(
+              // image: FileImage(
+              //     ),
               image: NetworkImage(
                   'https://media.discordapp.net/attachments/1033607346213105794/1298866706478923786/image.png?ex=671b1f62&is=6719cde2&hm=62e059e2049d46f8dc4817cda75f861bd87a1c888576441f89cf04e2070ead9f&=&format=webp&quality=lossless&width=486&height=662'),
-              height: 150, // Set a smaller fixed height for the image
-              width: 150, // Set a smaller fixed width for the image
+              height: 150, 
+              width: 150, 
+              fit: BoxFit.cover,
             ),
-            const SizedBox(height: 70),
+            const SizedBox(height: 70, width: 60),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.green,
-                minimumSize: const Size(400, 50),
+                minimumSize: const Size(300, 50),
               ),
               onPressed: () {
                 Navigator.push(
@@ -69,7 +72,8 @@ class landingPageState extends State<landingPage> {
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-                minimumSize: const Size(400, 50),
+                minimumSize: const Size(300, 50),
+                
               ),
               onPressed: () {
                 Navigator.push(
--- lib/login.dart
+++ lib/login.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:tubes_ppb/lupaPassword.dart';
 import 'package:tubes_ppb/landing.dart';
 import 'package:tubes_ppb/register.dart';
-import 'dashboard.dart';
 import 'package:tubes_ppb/verifikasi.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -44,21 +43,18 @@ class login extends StatelessWidget {
             children: <Widget>[
               
               Center(
-                child: const Text('Selamat Datang Kembali',
-                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+                child: Text('Selamat Datang Kembali',
+                    style: GoogleFonts.montserrat(fontSize: 24, fontWeight: FontWeight.bold)),
               ),
               const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  decoration: const InputDecoration(labelText: 'Email'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    return null;
-                  },
-                ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
               ),
               const SizedBox(height: 10),
               TextFormField(
@@ -73,16 +69,21 @@ class login extends StatelessWidget {
               ),
               const SizedBox(height: 20),
               Padding(
-                padding: const EdgeInsets.only(top: 10, bottom: 10),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const lupaPassword()),
-                    );
-                  },
-                  child: const Text('Lupa Password?'),
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const lupaPassword()),
+                        );
+                      },
+                      child: const Text('Lupa Password?'),
+                    ),
+                  ],
                 ),
               ),
               ElevatedButton(
@@ -95,7 +96,7 @@ class login extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => const Dashboard(),
+                        builder: (context) => verifikasi(),
                       ),
                     );
                   }
