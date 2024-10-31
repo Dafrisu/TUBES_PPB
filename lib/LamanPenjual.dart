bea586ef65af6c56fad256406ec10b57c97a46c9
Haikal Risnandar <haikalrisnandar82@gmail.com>
2024-10-31 13:03:31 +0700

    fix overwrite

--- lib/LamanPenjual.dart
+++ lib/LamanPenjual.dart
@@ -9,7 +9,6 @@ void main() {
 
 class Penjual extends StatelessWidget {
   const Penjual({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
