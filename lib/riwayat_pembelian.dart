eee8271c88d372b5997665c0ae513ae8f48853ff
darrylrambi <darrylrambi@gmail.com>
2024-11-07 15:21:11 +0700

    completed navbar

--- lib/riwayat_pembelian.dart
+++ lib/riwayat_pembelian.dart
@@ -2,10 +2,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:timelines/timelines.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
