dd81023ff611a8452100bc11d9eb2625672bc1e6
darrylrambi <darrylrambi@gmail.com>
2024-11-07 16:33:49 +0700

    fix navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tubes_ppb/animation.dart';
 import 'package:provider/provider.dart';
-import 'package:tubes_ppb/dashboard/dashboard.dart';
 import 'package:tubes_ppb/landing.dart';
 import 'package:tubes_ppb/login.dart';
 import 'userdata/user_provider.dart';
