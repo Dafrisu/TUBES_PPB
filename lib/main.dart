b45b990e73fb3eb7b9b98ff4eacdf1535c3d04a1
darryl <darrylrambi@gmail.com>
2024-12-27 20:07:02 +0700

    nyobain fetch dari API

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,19 @@ import 'package:tubes_ppb/landing.dart';
 import 'package:tubes_ppb/login.dart';
 import 'userdata/user_provider.dart';
 import 'animation.dart';
+import 'api/api_service.dart';
+import 'package:tubes_ppb/dashboard/dashboard_test.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    final data = await fetchData();
+    print(data);
+  } catch (e) {
+    print('Error: $e');
+  }
+  
   runApp(
     ChangeNotifierProvider(
       create: (context) =>
