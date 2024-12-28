5e9149a210912cf0bad4606b1f7b3b2c6b2d0f63
darryl <darrylrambi@gmail.com>
2024-12-28 15:44:07 +0700

    fix tipis

--- lib/dashboard/dashboard_full_makanan.dart
+++ lib/dashboard/dashboard_full_makanan.dart
@@ -65,7 +65,7 @@ class FullMakananPage extends StatelessWidget {
               final item = data[index];
               return ProductCard(
                 title: item['nama_barang'],
-                imageUrl: 'lib/assets_images/Minuman1.png',
+                imageUrl: item['image_url']
               );
             },
           );
