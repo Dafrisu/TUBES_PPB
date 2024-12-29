2bc9de3e59b1b0535de193aab4bb40add72ade2c
Dafrisu <daffaraymisuandi@gmail.com>
2024-12-29 23:05:31 +0700

    Merging

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -2,14 +2,16 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 Future<List<Map<String, dynamic>>> fetchProduk() async {
-  final response = await http.get(Uri.parse('https://umkmapi.azurewebsites.net/produk'));
+  final response =
+      await http.get(Uri.parse('https://umkmapi.azurewebsites.net/produk'));
 
   if (response.statusCode == 200) {
     // Decode the response body as a List
     final List<dynamic> data = jsonDecode(response.body);
-    
+
     // Cast the List<dynamic> to List<Map<String, dynamic>>
-    final List<Map<String, dynamic>> listProperties = data.cast<Map<String, dynamic>>();
+    final List<Map<String, dynamic>> listProperties =
+        data.cast<Map<String, dynamic>>();
 
     return listProperties;
   } else {
@@ -18,14 +20,16 @@ Future<List<Map<String, dynamic>>> fetchProduk() async {
 }
 
 Future<List<Map<String, dynamic>>> fetchMakanan() async {
-  final response = await http.get(Uri.parse('https://umkmapi.azurewebsites.net/produk/makanan'));
+  final response = await http
+      .get(Uri.parse('https://umkmapi.azurewebsites.net/produk/makanan'));
 
   if (response.statusCode == 200) {
     // Decode the response body as a List
     final List<dynamic> data = jsonDecode(response.body);
-    
+
     // Cast the List<dynamic> to List<Map<String, dynamic>>
-    final List<Map<String, dynamic>> listProperties = data.cast<Map<String, dynamic>>();
+    final List<Map<String, dynamic>> listProperties =
+        data.cast<Map<String, dynamic>>();
 
     return listProperties;
   } else {
@@ -34,14 +38,16 @@ Future<List<Map<String, dynamic>>> fetchMakanan() async {
 }
 
 Future<List<Map<String, dynamic>>> fetchMinuman() async {
-  final response = await http.get(Uri.parse('https://umkmapi.azurewebsites.net/produk/minuman'));
+  final response = await http
+      .get(Uri.parse('https://umkmapi.azurewebsites.net/produk/minuman'));
 
   if (response.statusCode == 200) {
     // Decode the response body as a List
     final List<dynamic> data = jsonDecode(response.body);
-    
+
     // Cast the List<dynamic> to List<Map<String, dynamic>>
-    final List<Map<String, dynamic>> listProperties = data.cast<Map<String, dynamic>>();
+    final List<Map<String, dynamic>> listProperties =
+        data.cast<Map<String, dynamic>>();
 
     return listProperties;
   } else {
@@ -50,17 +56,36 @@ Future<List<Map<String, dynamic>>> fetchMinuman() async {
 }
 
 Future<List<Map<String, dynamic>>> fetchMisc() async {
-  final response = await http.get(Uri.parse('https://umkmapi.azurewebsites.net/produk/misc'));
+  final response = await http
+      .get(Uri.parse('https://umkmapi.azurewebsites.net/produk/misc'));
 
   if (response.statusCode == 200) {
     // Decode the response body as a List
     final List<dynamic> data = jsonDecode(response.body);
-    
+
     // Cast the List<dynamic> to List<Map<String, dynamic>>
-    final List<Map<String, dynamic>> listProperties = data.cast<Map<String, dynamic>>();
+    final List<Map<String, dynamic>> listProperties =
+        data.cast<Map<String, dynamic>>();
 
     return listProperties;
   } else {
     throw Exception('Gagal load Misc UMKM');
+  }
+}
+
+//API buat riwayat Pembelian
+Future<List<Map<String, dynamic>>> fetchriwayatpembelian() async {
+  final response = await http
+      .get(Uri.parse('https://umkmapi.azurewebsites.net/getriwayatpesanan'));
+
+  if (response.statusCode == 200) {
+    final List<dynamic> data = jsonDecode(response.body);
+
+    final List<Map<String, dynamic>> listProperties =
+        data.cast<Map<String, dynamic>>();
+
+    return listProperties;
+  } else {
+    throw Exception('Gagal load Riwayat Pesanan');
   }
 }
