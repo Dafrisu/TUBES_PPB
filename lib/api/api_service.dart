f144615a46cc5edfe55aa9aa7c968f2f0e4c356b
darryl <darrylrambi@gmail.com>
2025-01-07 20:11:44 +0700

    add function fetch ulasan

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -59,3 +59,23 @@ Future<List<Map<String, dynamic>>> fetchDataUMKM() async {
     throw Exception('Failed to load data');
   }
 }
+
+Future<List<Map<String, dynamic>>> fetchUlasansByProdukId(int id) async {
+  // Construct the URL with the query parameter
+  final response = await http.get(
+    Uri.parse('https://umkmapi.azurewebsites.net/ulasan/$id'),
+  );
+
+  if (response.statusCode == 200) {
+    // Decode the response body as a List
+    final List<dynamic> data = jsonDecode(response.body);
+
+    // Cast the List<dynamic> to List<Map<String, dynamic>>
+    final List<Map<String, dynamic>> listProperties =
+        data.cast<Map<String, dynamic>>();
+
+    return listProperties;
+  } else {
+    throw Exception('Failed to load data');
+  }
+}
